384297ca828a46a920869ef98e36a94d29f81068
Tatia burdett <tatiaburdett@gmail.com>
2021-04-30 13:23:43 -0700

    create footer content

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,8 +4,13 @@ import './Footer.css'
 class Footer extends React.Component {
   render () {
     return (
-      <div>
-        Footer
+      <div className='footer'>
+        <p>Copyright &#169; 2021</p>
+        <p>Created by Tatia Burdett</p>
+        <ul className='footer-links'>
+          <li><a href='https://www.linkedin.com/in/tatjanaburdett/'>LinkedIn</a></li>
+          <li><a href='https://github.com/tatia-burdett'>GitHub</a></li>
+        </ul>
       </div>
     )
   }
