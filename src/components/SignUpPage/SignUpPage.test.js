755fc1dffdd6063a8ffc535c3ce18e4d761d41fa
Tatia burdett <tatiaburdett@gmail.com>
2021-04-30 13:34:37 -0700

    add tests

---
+++ src/components/SignUpPage/SignUpPage.test.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { BrowserRouter } from 'react-router-dom'
+import SignUpPage from './SignUpPage'
+
+it('renders without crashing', () => {
+  const div = document.createElement('div')
+  ReactDOM.render(
+    <BrowserRouter>
+      <SignUpPage />
+    </BrowserRouter>, 
+    div);
+  ReactDOM.unmountComponentAtNode(div)
+});
