fa85ae55c916158ffb59030e1d3b31af1a47e10f
Tatia burdett <tatiaburdett@gmail.com>
2021-04-30 13:36:57 -0700

    add section to landingpage

--- src/components/LandingPage/LandingPage.js
+++ src/components/LandingPage/LandingPage.js
@@ -12,6 +12,12 @@ class LandingPage extends React.Component {
           <h2>Your address history, when you need it!</h2>
           <GetStarted />
         </header>
+        <section>
+          <h3>Keep track of all your previous addresses</h3>
+          <div>
+            <p>Keeping track of your prior addresses is important when you get a background check. Often times, in these cases, we have to remember up to 7 years of addresses. If you are someone who moves a lot, sometimes these addresses can really pile up.</p>
+          </div>
+        </section>
       </div>
     )
   }
