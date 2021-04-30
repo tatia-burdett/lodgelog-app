46688f976404b5e1c075cfebaf0a06ea0d16188e
Tatia burdett <tatiaburdett@gmail.com>
2021-04-30 13:42:24 -0700

    add signup form

--- src/components/SignUpPage/SignUpPage.js
+++ src/components/SignUpPage/SignUpPage.js
@@ -1,10 +1,21 @@
 import React from 'react'
+import './SignUpPage.css'
 
 class SignUpPage extends React.Component {
   render () {
     return (
       <div>
-        Sign Up Page
+        <form>
+          <label>Name</label>
+          <input />
+          <label>Username</label>
+          <input />
+          <label>Password</label>
+          <input />
+          <label>Re-enter Password</label>
+          <input />
+          <button>Submit</button>
+        </form>
       </div>
     )
   }
