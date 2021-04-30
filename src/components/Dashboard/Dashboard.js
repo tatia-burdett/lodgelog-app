578d19b45897f0e11fc8bf383b63c72201220831
Tatia burdett <tatiaburdett@gmail.com>
2021-04-30 12:45:21 -0700

    add tests and css

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import './Dashboard.css'
 
 class AddressLog extends React.Component {
   render () {
--- src/components/LandingPage/LandingPage.js
+++ src/components/LandingPage/LandingPage.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import './LandingPage.css'
 
 class LandingPage extends React.Component {
   render () {
