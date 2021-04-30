15fc27a3ddca90a3b27b5ffc61a0680cd4558778
Tatia burdett <tatiaburdett@gmail.com>
2021-04-30 13:01:31 -0700

    add get started link

--- src/components/LandingPage/LandingPage.js
+++ src/components/LandingPage/LandingPage.js
@@ -1,11 +1,17 @@
 import React from 'react'
 import './LandingPage.css'
 
+import GetStarted from '../GetStarted/GetStarted'
+
 class LandingPage extends React.Component {
   render () {
     return (
       <div>
-        Hello Landing
+        <header>
+          <h1>LodgeLog</h1>
+          <h2>Your address history, when you need it!</h2>
+          <GetStarted />
+        </header>
       </div>
     )
   }
