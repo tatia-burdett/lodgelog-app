811364abe57f5df12c71c1af1d5cd5810b8f4831
Tatia burdett <tatiaburdett@gmail.com>
2021-05-01 10:38:23 -0700

    basic form styling

--- src/components/AddAddressPage/AddAddressPage.js
+++ src/components/AddAddressPage/AddAddressPage.js
@@ -4,20 +4,68 @@ import './AddAddressPage.css'
 class AddAddressPage extends React.Component {
   render () {
     return (
-      <div>
-        <form>
-          <label>Street Address</label>
-          <input />
-          <label>Unit Number</label>
-          <input />
-          <label>City</label>
-          <input />
-          <label>State</label>
-          <input />
-          <label>Zipcode</label>
-          <input />
-          <button>Submit</button>
-        </form>
+      <div className='add-address-page'>
+        <header className='add-address-page-header'>
+          <h1>Add an Address</h1>
+        </header>
+        <section className='add-address-page-section'>
+          <form className='add-address-page-form'>
+            <label htmlFor='street_addresss'>Street Address</label>
+            <input 
+              type='text'
+              id='street_address'
+              name='street_address'
+              placeholder='street_address'
+              required
+            />
+
+            <label htmlFor='unit'>Unit Number</label>
+            <input 
+              type='text'
+              id='unit'
+              name='unit'
+              placeholder='Unit or Apt #'
+            />
+
+            <label htmlFor='city'>City</label>
+            <input 
+              type='text'
+              id='city'
+              name='city'
+              placeholder='city'
+              required
+            />
+
+            <label htmlFor='state'>State</label>
+            <input 
+              type='text'
+              id='state'
+              name='state'
+              placeholder='state'
+              required
+            />
+
+            <label htmlFor='zipcode'>Zipcode</label>
+            <input 
+              type='text'
+              id='zipcode'
+              name='zipcode'
+              placeholder='zipcode'
+              required
+            />
+
+            <label htmlFor='current'>Current Address</label>
+            <input 
+              type='checkbox'
+              id='current'
+              name='current'
+            />
+
+            <button type='submit' className='submit-button'>
+              Submit
+            </button>
+          </form>
+        </section>
       </div>
     )
   }
