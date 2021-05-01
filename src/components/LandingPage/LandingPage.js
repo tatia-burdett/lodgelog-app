8e1519e10f4b443074b1744060da6b2e27341ec9
Tatia burdett <tatiaburdett@gmail.com>
2021-04-30 19:10:24 -0700

    add landingpage text

--- src/components/LandingPage/LandingPage.js
+++ src/components/LandingPage/LandingPage.js
@@ -6,16 +6,19 @@ import GetStarted from '../GetStarted/GetStarted'
 class LandingPage extends React.Component {
   render () {
     return (
-      <div>
-        <header>
+      <div className='landing-page'>
+        <header className='landing-header'>
           <h1>LodgeLog</h1>
           <h2>Your address history, when you need it!</h2>
           <GetStarted />
         </header>
-        <section>
-          <h3>Keep track of all your previous addresses</h3>
-          <div>
-            <p>Keeping track of your prior addresses is important when you get a background check. Often times, in these cases, we have to remember up to 7 years of addresses. If you are someone who moves a lot, sometimes these addresses can really pile up.</p>
+        <section className='landing-section'>
+          <h3>What is LodgeLog?</h3>
+          <div className='section-content-container'>
+            <p>LodgeLog is an address timeline for your own personal address history. It allows you to keep track of everywhere you have lived in an easily viewable way.</p>
+            <p>Have you ever applied for a job where they required your last 7 years of addresses? This can be a tough one to manage, especially if you have moved around a lot. According to Forbes, "The United States Census Bureau found that, between 2007 and 2012, Millennials accounted for about 24 percent of the total population of the U.S., but they made up over 43 percent of all movers." With younger generations traveling more often, and remote work becoming more commonplace, keeping record of previous homes can be both necessary and an enjoyable way to reminisce.</p>
+            <p>LodgeLog allows you to post new addresses to your address timeline, and scroll through it chronologically. Click the "get started" button above, or the login link in the upper right hard corner to start logging your lodges!</p>
+  
           </div>
         </section>
       </div>
