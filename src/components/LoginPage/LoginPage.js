eae104f90701827367328c7ca1855c2927d0acd7
Tatia burdett <tatiaburdett@gmail.com>
2021-05-01 10:24:38 -0700

    minor form and button styling

--- src/components/LoginPage/LoginPage.js
+++ src/components/LoginPage/LoginPage.js
@@ -5,17 +5,37 @@ import './LoginPage.css'
 class LoginPage extends React.Component {
   render () {
     return (
-      <div>
-        <form>
-          <label>Username</label>
-          <input />
-          <label>Password</label>
-          <input />
-          <button>
-            Login
-          </button>
-        </form>
-        <Link to='/sign-up'>Sign Up!</Link>
+      <div className='login-page'>
+        <header className='login-page-header'>
+          <h1>Sign in</h1>
+          <h2>Click the 'sign up' button to create an account</h2>
+        </header>
+        <section className='login-page-section'>
+          <form className='login-page-form'>
+            <label htmlFor='username'>Username</label>
+            <input 
+              type='text'
+              id='username'
+              name='username'
+              placeholder='username'
+              required
+            />
+
+            <label htmlFor='password'>Password</label>
+            <input 
+              type='text'
+              id='password'
+              name='password'
+              placeholder='password'
+              required
+            />
+
+            <button type='submit' className='submit-button'>
+              Login
+            </button>
+          </form>
+          <Link to='/sign-up'>Sign Up!</Link>
+        </section>
       </div>
     )
   }
