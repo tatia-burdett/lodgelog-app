8d4e8f5cd9c7dd81e9486616b481a0ee0311a04b
Tatia burdett <tatiaburdett@gmail.com>
2021-05-01 10:31:12 -0700

    basic form styling

--- src/components/SignUpPage/SignUpPage.js
+++ src/components/SignUpPage/SignUpPage.js
@@ -4,18 +4,53 @@ import './SignUpPage.css'
 class SignUpPage extends React.Component {
   render () {
     return (
-      <div>
-        <form>
-          <label>Name</label>
-          <input />
-          <label>Username</label>
-          <input />
-          <label>Password</label>
-          <input />
-          <label>Re-enter Password</label>
-          <input />
-          <button>Submit</button>
-        </form>
+      <div className='signup-page'>
+        <header className='signup-page-header'>
+          <h1>Sign Up</h1>
+        </header>
+        <section className='signup-page-section'>
+          <form className='signup-page-form'>
+            <label htmlFor='name'>Name</label>
+            <input 
+              type='text'
+              id='name'
+              name='name'
+              placeholder='name'
+              required
+            />
+
+            <label htmlFor='username'>Username</label>
+            <input 
+              type='text'
+              id='username'
+              name='username'
+              placeholder='username'
+              required
+            />
+
+            <label htmlFor='password'>Password</label>
+            <input 
+              type='text'
+              id='password'
+              name='password'
+              placeholder='password'
+              required
+            />
+
+            <label htmlFor='repassword'>Re-enter Password</label>
+            <input 
+              type='text'
+              id='repassword'
+              name='repassword'
+              placeholder='repeat password'
+              required
+            />
+
+            <button type='submit' class='submit-button'>
+              Submit
+            </button>
+          </form>
+        </section>
       </div>
     )
   }
