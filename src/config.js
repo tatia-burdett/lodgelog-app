f28ea245824a27de56db719b84d93e7fa913c47b
Tatia burdett <tatiaburdett@gmail.com>
2021-05-05 10:27:40 -0700

    add token-service file

--- src/config.js
+++ src/config.js
@@ -1,5 +1,6 @@
 const config = {
-  API_ENDPOINT: 'https://calm-temple-49438.herokuapp.com/api/'
+  API_ENDPOINT: 'https://calm-temple-49438.herokuapp.com/api/',
+  TOKEN_KEY: 'lodgelog-client-auth-token'
 }
 
 export default config
