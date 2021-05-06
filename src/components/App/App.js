4a782ea2ae0c925c6cecdf45aee71cd01fad35f4
Tatia burdett <tatiaburdett@gmail.com>
2021-05-06 09:07:49 -0700

    move files

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,16 +1,15 @@
 import React from 'react';
 import { Route } from 'react-router-dom'
 import './App.css'
-import AddressContext from './AddressContext'
-import FetchApiService from './services/fetch-api-service'
+import AddressContext from '../../AddressContext'
 
-import NavBar from './components/NavBar/NavBar'
-import LandingPage from './components/LandingPage/LandingPage'
-import Dashboard from './components/Dashboard/Dashboard'
-import Login from './components/LoginPage/LoginPage'
-import SignUpPage from './components/SignUpPage/SignUpPage'
-import AddAddressPage from './components/AddAddressPage/AddAddressPage'
-import Footer from './components/Footer/Footer'
+import NavBar from '../NavBar/NavBar'
+import LandingPage from '../LandingPage/LandingPage'
+import Dashboard from '../Dashboard/Dashboard'
+import Login from '../LoginPage/LoginPage'
+import SignUpPage from '../SignUpPage/SignUpPage'
+import AddAddressPage from '../AddAddressPage/AddAddressPage'
+import Footer from '../Footer/Footer'
 
 class App extends React.Component {
   constructor(props) {
