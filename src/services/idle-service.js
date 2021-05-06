752159ae1cc0b606c9a5247d51382fc96171b9f3
Tatia burdett <tatiaburdett@gmail.com>
2021-05-06 10:51:34 -0700

    add idle service

---
+++ src/services/idle-service.js
@@ -0,0 +1,24 @@
+let _timeoutId
+let _idleCallback = null
+let _notIdleEvents = ['mousedown', 'mousemove', 'keypress', 'scroll', 'touchstart']
+let _FIVE_MINUTES_IN_MS = 5 * 60 * 1000
+
+const IdleService = {
+  setIdleCallback(idleCallback) {
+    _idleCallback = idleCallback
+  },
+
+  resetIdleTimer(ev) {
+    console.log('event:', ev.type)
+    clearTimeout(_timeoutId)
+    _timeoutId = setTimeout(_idleCallback, _FIVE_MINUTES_IN_MS)
+  },
+
+  registerIdleTimerResets() {
+    _notIdleEvents.forEach(event => 
+      document.removeEventListener(event, IdleService.resetIdleTimer, true)
+    )
+  }
+}
+
+export default IdleService
