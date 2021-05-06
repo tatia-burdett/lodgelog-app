7346a5d645c7494c092e19987beaca6138630b13
Tatia burdett <tatiaburdett@gmail.com>
2021-05-06 11:03:09 -0700

    add auth-api-service

--- src/services/auth-api-service.js
+++ src/services/auth-api-service.js
@@ -34,7 +34,35 @@ const AuthApiService = {
       .then(res => {
         TokenService.saveAuthToken(res.authToken)
         IdleService.registerIdleTimerResets()
-        TokenService.
+        TokenService.queueCallbackBeforeExpiry(() => {
+          AuthApiService.postRefreshToken()
+        })
+        return res
+      })
+  },
+
+  postRefreshToken() {
+    return fetch(`${config.API_ENDPOINT}/auth/refresh`, {
+      method: 'POST',
+      headers: {
+        'authorization': `Bearer ${TokenService.getAuthToken()}`
+      }
+    })
+      .then(res => {
+        (!res.ok)
+          ? res.json().then(e => Promise.reject(e))
+          : res.json()
+      })
+      .then(res => {
+        TokenService.saveAuthToken(res.authToken)
+        TokenService.queueCallbackBeforeExpiry(() => {
+          AuthApiService.postRefreshToken()
+        })
+        return res
+      })
+      .catch(err => {
+        console.log('refresh token request error')
+        console.log(err)
       })
   }
 }
