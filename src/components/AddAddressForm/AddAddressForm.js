b0021d82312ba86f03d890294cee6f96d80f0b70
Tatia burdett <tatiaburdett@gmail.com>
2021-05-07 14:11:42 -0700

    add fetch POST

--- src/components/AddAddressForm/AddAddressForm.js
+++ src/components/AddAddressForm/AddAddressForm.js
@@ -1,10 +1,70 @@
 import React from 'react'
+import config from '../../config'
+import AddressContext from '../../AddressContext'
 
 class AddAddressForm extends React.Component {
-  
+  static contextType = AddressContext
+
+  state = { error: null }
+
+  handleSubmit = (e) => {
+    e.preventDefault()
+    this.setState({ error: null })
+    const { from_date, to_date, street_address, unit, city, abb_state, zipcode, current } = e.target
+    const userId = this.context.currentUser
+
+    const requestOptions = {
+      method: 'POST',
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify({
+        from_date: `${from_date.value}`,
+        to_date: `${to_date.value}`,
+        street_address: `${street_address.value}`,
+        unit: `${unit.value}`,
+        city: `${city.value}`,
+        abb_state: `${abb_state.value}`,
+        zipcode: `${zipcode.value}`,
+        current: `${current.value}`,
+        userid: `${userId}`
+      })
+    }
+
+    fetch(`${config.API_ENDPOINT}/address`, requestOptions)
+      .then(res => {
+        if (!res.ok) {
+          throw new Error('Ooops, something went wrong!')
+        }
+        return res.json()
+      })
+      .then(() => {
+        this.props.history.goBack()
+      })
+      .catch(error => {
+        this.setState({ error })
+      })
+  }
+
   render() {
     return (
-      <form className='add-address-page-form'>
+      <form 
+      className='add-address-page-form'
+      onSubmit={this.handleSubmit}
+      >
+        <label htmlFor='from_date'>From</label>
+        <input 
+          type='text'
+          id='from_date'
+          name='from_date'
+          required
+        />
+
+        <label htmlFor='to_date'>To</label>
+        <input 
+          type='text'
+          id='to_daye'
+          name='to_date'
+        />
+
         <label htmlFor='street_addresss'>Street Address</label>
         <input 
           type='text'
@@ -31,11 +91,11 @@ class AddAddressForm extends React.Component {
           required
         />
 
-        <label htmlFor='state'>State</label>
+        <label htmlFor='abb_state'>State</label>
         <input 
           type='text'
-          id='state'
-          name='state'
+          id='abb_state'
+          name='abb_state'
           placeholder='state'
           required
         />
@@ -54,6 +114,7 @@ class AddAddressForm extends React.Component {
           type='checkbox'
           id='current'
           name='current'
+          value={false}
         />
 
         <button type='submit' className='submit-button'>
